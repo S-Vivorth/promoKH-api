696e171ef04566501d63b981084ee20971cb3177
DoungChan <sreoun.doungchan19@kit.edu.kh>
2023-06-01 23:46:33 +0700

    add category controller

---
+++ src/main/java/com/kit/promokhapi/controllers/CategoryController.java
@@ -0,0 +1,24 @@
+package com.kit.promokhapi.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.kit.promokhapi.models.Category;
+import com.kit.promokhapi.service.CategorySerivce;
+
+@RestController
+public class CategoryController {
+
+    @Autowired
+    CategorySerivce service;
+    
+    // get all categories
+    @GetMapping("promo_kh/category")
+    public List<Category> getAllCategories() {
+        return service.getAllCategories();
+    }
+    
+}

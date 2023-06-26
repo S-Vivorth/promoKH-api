1089a79ab13af8c38a951a204c67a91b32a44234
Vivorth <60089316+Vivorth@users.noreply.github.com>
2023-06-26 20:34:21 +0700

    Revert "Refactor: clean up code"

--- src/main/java/com/kit/promokhapi/repository/PromotionRepository.java
+++ src/main/java/com/kit/promokhapi/repository/PromotionRepository.java
@@ -1,15 +1,22 @@
 package com.kit.promokhapi.repository;
 
 import com.kit.promokhapi.models.Promotion;
+import com.kit.promokhapi.models.User;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface PromotionRepository extends MongoRepository<Promotion, String> {
 
     Page<Promotion> findByCategoryId(String categoryId, Pageable pageable);
 
     Page<Promotion> findByTitleContainingIgnoreCase(String keyword, Pageable pageable);
-  
+
+
+
+
 }

f9dbca3a45b90ef16f422bc62cae607d2d2a7198
Sen Rith <senrithney2@gmail.com>
2023-07-13 19:23:58 +0700

    feat: search by location

--- src/main/java/com/kit/promokhapi/repository/PromotionRepository.java
+++ src/main/java/com/kit/promokhapi/repository/PromotionRepository.java
@@ -5,11 +5,13 @@ import com.kit.promokhapi.models.Promotion;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 public interface PromotionRepository extends MongoRepository<Promotion, String> {
 
     Page<Promotion> findByCategoryId(String categoryId, Pageable pageable);
 
+    @Query("{'$or': [{'title': {$regex: ?0, $options: 'i'}}, {'location': {$regex: ?0, $options: 'i'}}]}")
     Page<Promotion> findByTitleContainingIgnoreCase(String keyword, Pageable pageable);
 
 }
